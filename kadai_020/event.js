cb1cb587694a549e520e1da5fd5cd129053de3f8
Afro0605 <68795183+Afro0605@users.noreply.github.com>
2025-03-23 17:10:14 +0900

    kadai_020をアップデート

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const beforeBtn = document.getElementById('btn');
+beforeBtn.addEventListener('click', () =>{
+    const afterBtn = document.getElementById('text');
+    afterBtn.textContent = 'ボタンをクリックしました'
+})
